c2fc1f61fe3dd6f95ca3f5aeb8a2e29400921c9d
Adam Greig <adam@adamgreig.com>
2011-01-23 20:41:56 +0000

    Added CMake and Google Tests

---
+++ src/main.cc
@@ -0,0 +1,16 @@
+// IDP L108 Software
+// Copyright 2011 Adam Greig & Jon Sowman
+//
+// main.cc
+// Program entry point
+
+#include <iostream>
+
+// Program entry point and main loop
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+
+    // Exit
+    return 0;
+}
