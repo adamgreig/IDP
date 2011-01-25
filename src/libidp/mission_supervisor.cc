7085a83174997e3ba3f1b9cea98166ac7f5194f8
A.G. Greig <ag611@ew115.eng.cam.ac.uk>
2011-01-25 09:28:21 +0000

    time many iterations of the test instruction

--- src/libidp/mission_supervisor.cc
+++ src/libidp/mission_supervisor.cc
@@ -9,19 +9,32 @@
 #include <iostream>
 #include <robot_instr.h>
 #include <robot_link.h>
+#include <stopwatch.h>
 
 namespace IDP {
     MissionSupervisor::MissionSupervisor()
     {
+        // Initialise link
         std::cout << "Initialising a MissionSupervisor..." << std::endl;
         robot_link rlink;
         if(!rlink.initialise(39)) {
             std::cout << "Error initialising rlink" << std::endl;
+            return;
         }
-        int val = rlink.request(TEST_INSTRUCTION);
-        std::cout << "Finished test request, val = " << val << std::endl;
 
-        rlink.print_errs("[Error] ");
+        // Time many iterations of the test instruction
+        int i;
+        stopwatch sw;
+        sw.start();
+        for(i=0; i<1024; i++) {
+            rlink.request(TEST_INSTRUCTION);
+        }
+        int time = sw.stop();
+
+        std::cout << "Ran 1024 iterations of TEST_INSTRUCTION:" << std::endl;
+        std::cout << "\tTotal time: " << time << "ms" << std::endl;
+        std::cout << "\tTime per instruction: " << time / 1024 << "ms";
+        std::cout << std::endl;
     }
 }
 
