f4edade795747a37eec6dff8b48ee404ec4467bd
Jon Sowman <jon.sowman@gmail.com>
2011-02-02 10:57:26 +0000

    Added status watchdog class implementation beginnings and modified line following a bit

---
+++ src/libidp/status_watchdog.cc
@@ -0,0 +1,17 @@
+// IDP
+// Copyright 2011 Adam Greig & Jon Sowman
+//
+// status_watchdog.cc
+// Status Watchdog class implementation
+//
+
+#include "status_watchdog.h"
+
+namespace IDP {
+    /**
+     * Read the STATUS register of the microcontroller and return the value
+     * \returns The error encountered, if any 
+     */
+    const int check() const {
+    }
+}
