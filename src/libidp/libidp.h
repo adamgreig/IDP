a86a3e9481bef9e675892b6636c965c0a2ebe323
Jon Sowman <jon@hexoc.com>
2011-02-02 19:40:04 +0000

    Main page and intro etc etc etc

--- src/libidp/libidp.h
+++ src/libidp/libidp.h
@@ -4,6 +4,13 @@
 // libidp.h
 // Header file for external inclusion. Pulls in all library headers.
 
+/**
+ * \mainpage
+ * Integrated Design Project Team L108 Source Code Documentation.
+ *
+ * See \ref IDP "IDP Namespace" for API.
+ */
+
 #pragma once
 #ifndef LIBIDP_LIBIDP_H
 #define LIBIDP_LIBIDP_H
