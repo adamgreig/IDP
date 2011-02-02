575fc7e2f2d095f92fda18637b937b5146e27cf9
Jon Sowman <jon.sowman@gmail.com>
2011-02-02 10:15:54 +0000

    Added header files

---
+++ src/libidp/clamp_control.h
@@ -0,0 +1,19 @@
+// IDP
+// Copyright 2011 Adam Greig & Jon Sowman
+//
+// .h
+// class definition
+//
+
+#pragma once
+#ifndef LIBIDP_H
+#define LIBIDP_H
+
+#include <robot_link.h>
+
+namespace IDP {
+
+}
+
+#endif /* LIBIDP_H */
+
